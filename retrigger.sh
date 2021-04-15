f923320a600fa9361bab84dacb9dc2771c299eeb
Lari Hotari <lari@hotari.net>
2021-04-15 11:36:21 +0300

    Update the retrigger script

--- retrigger.sh
+++ retrigger.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo ' ' >> README.md && git add README.md && git commit --amend --no-edit && git push -f origin main
+git commit --amend --no-edit --allow-empty && git push -f origin main
